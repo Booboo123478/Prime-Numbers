6ebb1e2c0146c0eb293a3e011fd6a31307644d13
Booboo123478 <marcantoinetissot7@gmail.com>
2025-11-10 11:31:16 +0100

    Test 9 - faillure

--- src/test/java/PrimeCheckerTest.java
+++ src/test/java/PrimeCheckerTest.java
@@ -30,4 +30,30 @@ public class PrimeCheckerTest {
     public void test4IsNotPrime() {
         assertFalse(primeChecker.isPrime(4), "4 should not be prime");
     }
+
+    @Test
+    public void test5IsPrime() {
+        assertTrue(primeChecker.isPrime(5), "5 should not be prime");
+    }
+
+    @Test
+    public void test6IsNotPrime() {
+        assertFalse(primeChecker.isPrime(6), "6 should not be prime");
+    }
+
+    @Test
+    public void test7IsPrime() {
+        assertTrue(primeChecker.isPrime(7), "7 should be prime");
+    }
+
+    @Test
+    public void test8NotIsPrime() {
+        assertFalse(primeChecker.isPrime(8), "8 should not be prime");
+    }
+
+    @Test
+    public void test9IsPrime() {
+        assertFalse(primeChecker.isPrime(9), "9 should not be prime");
+    }
 }
+
