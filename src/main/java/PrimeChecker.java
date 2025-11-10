e368204d98662ed6238a3bd224d7032d6a9f683f
Booboo123478 <marcantoinetissot7@gmail.com>
2025-11-10 10:36:00 +0100

    Ini project + Test 0 failure

---
+++ src/main/java/PrimeChecker.java
@@ -0,0 +1,17 @@
+public class PrimeChecker {
+
+    public boolean isPrime(int n) {
+
+        for (int i = 0; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/PrimeCheckerTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
+
+public class PrimeCheckerTest {
+    
+    private final PrimeChecker primeChecker = new PrimeChecker();
+
+    @Test
+    public void test0IsNotPrime() {
+        assertFalse(primeChecker.isPrime(0), "0 should not be prime");
+    }
+ 
+}
