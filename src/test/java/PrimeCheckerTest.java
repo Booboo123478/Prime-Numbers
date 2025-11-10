045cd0bd6a9ee913d97fd8111c5220cfda128c19
Booboo123478 <marcantoinetissot7@gmail.com>
2025-11-10 11:51:37 +0100

    Test 25 Failure

--- src/test/java/PrimeCheckerTest.java
+++ src/test/java/PrimeCheckerTest.java
@@ -52,8 +52,89 @@ public class PrimeCheckerTest {
     }
 
     @Test
-    public void test9IsPrime() {
+    public void test9NotIsPrime() {
         assertFalse(primeChecker.isPrime(9), "9 should not be prime");
     }
+
+    @Test
+    public void test10NotIsPrime() {
+        assertFalse(primeChecker.isPrime(10), "10 should not be prime");
+    }
+
+    @Test
+    public void test11IsPrime() {
+        assertTrue(primeChecker.isPrime(11), "11 should not be prime");
+    }
+
+    @Test
+    public void test12NotIsPrime() {
+        assertFalse(primeChecker.isPrime(12), "12 should not be prime");
+    }
+
+    @Test
+    public void test13IsPrime() {
+        assertTrue(primeChecker.isPrime(13), "13 should not be prime");
+    }
+
+    @Test
+    public void test14NotIsPrime() {
+        assertFalse(primeChecker.isPrime(14), "14 should not be prime");
+    }
+
+    @Test
+    public void test15NotIsPrime() {
+        assertFalse(primeChecker.isPrime(15), "15 should not be prime");
+    }
+
+    @Test
+    public void test16NotIsPrime() {
+        assertFalse(primeChecker.isPrime(16), "16 should not be prime");
+    }
+
+    @Test
+    public void test17IsPrime() {
+        assertTrue(primeChecker.isPrime(17), "17 should be prime");
+    }
+
+    @Test
+    public void test18NotIsPrime() {
+        assertFalse(primeChecker.isPrime(18), "18 should not be prime");
+    }
+
+    @Test
+    public void test19IsPrime() {
+        assertTrue(primeChecker.isPrime(19), "19 should be prime");
+    }
+
+    @Test
+    public void test20NotIsPrime() {
+        assertFalse(primeChecker.isPrime(20), "20 should not be prime");
+    }
+
+    @Test
+    public void test21NotIsPrime() {
+        assertFalse(primeChecker.isPrime(21), "21 should not be prime");
+    }
+
+    @Test
+    public void test22NotIsPrime() {
+        assertFalse(primeChecker.isPrime(22), "22 should not be prime");
+    }
+
+    @Test
+    public void test23IsPrime() {
+        assertTrue(primeChecker.isPrime(23), "23 should be prime");
+    }
+
+    @Test
+    public void test24NotIsPrime() {
+        assertFalse(primeChecker.isPrime(24), "24 should not be prime");
+    }
+
+    @Test
+    public void test25NotIsPrime() {
+        assertFalse(primeChecker.isPrime(25), "25 should not be prime");
+    }
+
 }
 
