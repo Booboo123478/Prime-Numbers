2c2b47f2c0502720f32aa84c57c4288892b2ce90
Booboo123478 <marcantoinetissot7@gmail.com>
2025-11-10 11:54:39 +0100

    test 25 refactor

--- src/main/java/PrimeChecker.java
+++ src/main/java/PrimeChecker.java
@@ -4,8 +4,10 @@ public class PrimeChecker {
         if (n<=1){
             return false;
         }
-        if (n%2==0 && n!=2 || n%3==0 && n!=3 || n%5==0 && n!=5){
-            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
         }
 
         return true;
