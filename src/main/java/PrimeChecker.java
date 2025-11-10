902ab1a2097881211067add20b6f9a649f49e927
Booboo123478 <marcantoinetissot7@gmail.com>
2025-11-10 10:38:55 +0100

    Test 0 success

--- src/main/java/PrimeChecker.java
+++ src/main/java/PrimeChecker.java
@@ -1,7 +1,9 @@
 public class PrimeChecker {
 
     public boolean isPrime(int n) {
-
+        if (n==0){
+            return false;
+        }
         for (int i = 0; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
