0b90773d32307863bca792888fbc416ccf061e6d
Booboo123478 <marcantoinetissot7@gmail.com>
2025-11-10 10:42:17 +0100

    Refacto test 0

--- src/main/java/PrimeChecker.java
+++ src/main/java/PrimeChecker.java
@@ -4,7 +4,7 @@ public class PrimeChecker {
         if (n==0){
             return false;
         }
-        for (int i = 0; i <= Math.sqrt(n); i++) {
+        for (int i = 1; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
