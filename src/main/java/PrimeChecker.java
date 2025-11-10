aad2e608acab4179929de46a242df692ed8c7b49
Booboo123478 <marcantoinetissot7@gmail.com>
2025-11-10 11:33:27 +0100

    test 9 sucess

--- src/main/java/PrimeChecker.java
+++ src/main/java/PrimeChecker.java
@@ -7,6 +7,9 @@ public class PrimeChecker {
         if (n%2==0 && n!=2){
             return false;
         }
+        if (n%3==0 && n!=3){
+            return false;
+        }
         return true;
     }
 
